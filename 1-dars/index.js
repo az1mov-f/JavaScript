f3a1f49d8df1016aaeb4c186509cd632bbf19d44
FozilAzimov <foziljonweb@gmail.com>
2025-10-08 18:02:06 +0500

    changed

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -3,3 +3,23 @@ console.log("Hi Github");
 // 1. git init
 
 // 2. git remote add origin https://github.com/az1mov-f/JavaScript.git
+
+// 3. git add .
+
+// 4. git commit -m "comentariya"
+
+// 5. git config --global user.email "eshmat@gmail.com"
+
+// 6. git config --global user.name "eshmat"
+
+// 7. git commit -m "comentariya"
+
+// 8. git push origin master
+
+// ========================================
+
+// 1. git add .
+
+// 2. git commit -m "changed"
+
+// 3. git push origin master
