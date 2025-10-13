b1cd8bcee3db90585620d7ac316064eb1fe0128a
FozilAzimov <foziljonweb@gmail.com>
2025-10-13 18:00:03 +0500

    2-dars. Veriables

---
+++ 2-dars/index.js
@@ -0,0 +1,54 @@
+// console.log("Hi JavaScript!");
+// console.log("Hi Css!");
+// console.log("Hi Html!");
+
+// console.log("Shaxjahon 1");
+// console.log("Shaxjahon 2");
+// console.log("Shaxjahon 3");
+
+// var matn = "dcjscjsbcsjhcbdhjcshdcbjsdhcsbjdchdbsjcbshdbcjsdchbjsdhbcjshbcjshcbsjdcbsjdcbsjdcsbdcjshcbsjdhcbsjc";
+
+// let matn =
+//   "dcjscjsbcsjhcbdhjcshdcbjsdhcsbjdchdbsjcbshdbcjsdchbjsdhbcjshbcjshcbsjdcbsjdcbsjdcsbdcjshcbsjdhcbsjc";
+
+// const matn =
+//   "dcjscjsbcsjhcbdhjcshdcbjsdhcsbjdchdbsjcbshdbcjsdchbjsdhbcjshbcjshcbsjdcbsjdcbsjdcsbdcjshcbsjdhcbsjc";
+
+// var, let, const
+
+// console.log(matn);
+
+// console.log(matn);
+
+// var name = "Eshmat";
+
+// var name = "Toshmat";
+
+// console.log(name);
+// ======================
+
+// let phone = 998901112233;
+
+// let phone = 998999998877;
+
+// console.log(phone);
+
+// =========================
+
+// const name = "Eshmat";
+
+// const name = "Toshmat";
+
+// name = "Toshmat";
+
+// console.log(name);
+
+// var, let, const
+
+// let name;
+
+// console.log(name);
+
+// let userNameAge = "Eshmat";
+
+// ====================================
