0995e26554dccc34ed95de4715ed56fc1362ac6a
FozilAzimov <foziljonweb@gmail.com>
2025-10-15 17:58:50 +0500

    3-dars

---
+++ 3-dars/index.js
@@ -0,0 +1,52 @@
+// let name = "Ali";
+
+// console.log(name);
+
+// name = "Vali";
+
+// console.log(name);
+
+// const data = 100;
+// data = 200;
+
+// let a = "Shohjaxon";
+
+// a = "Muhammadali";
+
+// console.log(a);
+
+// let asd = "ckjdscndkscnsjdcksdnjcksdcjsdkcsc";
+
+// console.log(asd);
+
+// Alert()
+
+// alert("Assalom !");
+// let data = alert("Assalom !");
+// console.log(data);
+// Ekranda xabar chiqarish uchun kerak bo'ladi.
+
+// Prompt()
+// let age = prompt("Yoshingizni kiriting");
+// console.log(age);
+// Ham message chiqarish va User dan ma'lumot olish uchun ishlatilar ekan.
+
+// Confirm()
+
+// let savol = confirm("Sizni yoshingiz 18 dan kattami?");
+
+// console.log(savol);
+
+// =========================
+
+// let data = prompt("Nimadir");
+
+// console.log(data);
+
+let year = prompt("O'zingizni Yilizni kiriting");
+let one_age = 2025 - year;
+
+let year2 = prompt("Otangizni Yilini kiriting");
+let two_age = 2025 - year2;
+
+console.log((two_age + one_age) / 2);
