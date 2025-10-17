5e79880f3717944ca86bef655cedf24501617a6b
FozilAzimov <foziljonweb@gmail.com>
2025-10-17 17:39:22 +0500

    4-dars

---
+++ 4-dars/index.js
@@ -0,0 +1,66 @@
+// var name = "Alisher";
+// console.log(name);
+
+// let lastName = "Muhammadaliyev";
+// console.log(lastName);
+
+// const data = 100;
+
+// console.log(data);
+
+// ======================================
+
+// let var = 100 // Error
+// let function = 100 // Error
+// let return = 100 // Error
+// let if = 100 // Error
+// let else = 100 // Error
+// let lastName = 100; // Error
+
+// ===================================================
+
+// 2 - dars.
+
+// let name = "Ali";
+// let age = 20;
+// let isMerrid = false;
+// Data type - Ma'lumot turi
+
+// 1. Primitive :
+// 1.1 - string  : "Ali", '378372', `salom2025`
+// 1.2 - number  : +-2^53, 1, 2, 3, 4, 5.3, 10.8
+// 1.3 - bigint : 9007199254740993n = 2 ** 53 + 1
+// 1.4 - boolean : true, false
+// 1.5 - symbol : bir - biriga o'xshamas data yasab beradi.
+// 1.6 - null  :  yo'q ma'lumot
+// 1.7 - undefined :  yo'q ma'lumot
+// let ism = "Ali";
+// console.log(ism);
+// ism = null;
+// console.log(ism);
+
+// 2. Non Primitive
+// Objects --> Object, Array, Function
+
+// 2.1 - Object : {key: value, key2: value2}
+// let data = { name: "Ali", age: 20 };
+// 2.2 - Array : [1, "Ali", true, false, null, undefined, [2, 3]]
+
+// 2.3 - Function :
+
+// 1. function xisobla(){}
+
+// 2. let xisobla2 = function(){}
+
+// 3. let xisobla3 = ()=>{}
+
+// =====================================================
+
+// let name = "Eshmat";
+// console.log(typeof name, name);
+
+// let person = {
+//   name: "Eshmat",
+//   age: 20,
+//   address: "Kosonsoy, katta masjid",
+// };
